7c63d313e13b8eced6cc5b1a460c0eaef67c4b45
Nightang Tang <nightang@gmail.com>
2017-05-26 11:36:30 +0800

    Initialize Project
    Change-Id: I0000000000000000000000000000000000000004
    Signed-off-by: Nightang Tang <nightang@gmail.com>

---
+++ sqlite-mybatis-model-generator/src/main/java/org/nightang/db/DBGenLauncher.java
@@ -0,0 +1,79 @@
+package org.nightang.db;
+
+import java.io.File;
+
+import org.nightang.db.mybatis.DBModelGenerator;
+import org.nightang.db.sqlite.SQLiteDatabaseGenerator;
+
+public class DBGenLauncher {
+	
+	public static final String OUTPUT_DIR = "output";
+	
+	public static final String INPUT_DIR = "input_sample";
+
+	public static void main(String[] args) throws Exception {
+		
+		// Handle Input Parameters
+		String inputDirectory = INPUT_DIR;
+		String myBatisConfigFileName = "mybatis-generator-config.xml";
+		String schemaDDLFileName = "schema-ddl.sql";
+		String dbfileName = "database.db";
+		if(args.length == 0) {
+			System.out.println("No Parameter. Use Sample Setting.");
+		} else if(args.length == 3) {
+			inputDirectory = args[0];
+			myBatisConfigFileName = args[1];
+			schemaDDLFileName = args[2];
+		} else if(args.length == 4) {
+			inputDirectory = args[0];
+			myBatisConfigFileName = args[1];
+			schemaDDLFileName = args[2];
+			dbfileName = args[3];			
+		} else {
+			throw new IllegalArgumentException("Invalid Input Parameters.");
+		}
+		printOut(inputDirectory, myBatisConfigFileName, schemaDDLFileName);
+		
+		// Clear Previous Files & Folder
+		File dir = new File(OUTPUT_DIR);
+		if(dir.exists() && dir.isDirectory()) {
+			File[] files = dir.listFiles();
+			for(File file : files) {
+				deleteFiles(file);
+			}
+		} else {
+			dir.mkdir();
+		}
+		
+		// Create Database
+		String dbFilePath = OUTPUT_DIR + File.separator + dbfileName;
+		SQLiteDatabaseGenerator dbGen = new SQLiteDatabaseGenerator();
+		dbGen.createDatabaseAndSchema(dbFilePath, inputDirectory + File.separator + schemaDDLFileName);
+			
+		// Create DB Model Code
+		DBModelGenerator modelGen = new DBModelGenerator();
+		modelGen.generateDBModel(inputDirectory + File.separator + myBatisConfigFileName, dbFilePath);
+
+	}
+	
+	private static void printOut(String s1, String s2, String s3) {
+		System.out.println("===============================================");
+		System.out.println("> Input Folder       : " + s1);
+		System.out.println("> Mybatis Config File: " + s2);
+		System.out.println("> Schema DDL File    : " + s3);
+		System.out.println("===============================================");
+	}
+	
+	private static void deleteFiles(File file) {
+		if(file.isDirectory()) {
+			File[] subFiles = file.listFiles();
+			for(File subFile : subFiles) {
+				deleteFiles(subFile);
+			}
+			file.delete();
+		} else {
+			file.delete();
+		}
+	}
+
+}
--- sqlite-mybatis-model-generator/src/main/java/org/nightang/db/mybatis/DBModelGenerator.java
+++ sqlite-mybatis-model-generator/src/main/java/org/nightang/db/mybatis/DBModelGenerator.java
@@ -6,26 +6,33 @@ import java.util.List;
 
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.Context;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-public class DBGen {
-
-	public static void main(String[] args) throws Exception {
-		System.out.println("Start Generate...");
+public class DBModelGenerator {
+	
+	public void generateDBModel(String configPath, String dbFilePath) throws Exception {
+		System.out.println("Start Generate DB Model Code ......");
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
 		
 		// Set File Name
-		File configFile = new File("config/generatorConfig.xml");
-		
+		File configFile = new File(configPath);
 		
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
+		
+		// Replace ConnectionURL
+		List<Context> cxts = config.getContexts();
+		for(Context cxt : cxts) {
+			String url = cxt.getJdbcConnectionConfiguration().getConnectionURL();
+			cxt.getJdbcConnectionConfiguration().setConnectionURL(url.replace("{0}", dbFilePath));
+		}		
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
 		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
 		myBatisGenerator.generate(null);
-		System.out.println("Finished.");
+		System.out.println("Finished");
 	}
 
 }
